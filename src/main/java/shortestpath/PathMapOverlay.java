e14e1c9fa0127be2b064f56208d307accb9e885a
Montinevra <23004878+montinevra@users.noreply.github.com>
2022-02-21 18:43:18 -0700

    Fix transport lines drawing outside the map window

--- src/main/java/shortestpath/PathMapOverlay.java
+++ src/main/java/shortestpath/PathMapOverlay.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Area;
+import java.awt.geom.Line2D;
 import java.util.List;
 import net.runelite.api.Client;
 import net.runelite.api.Point;
@@ -49,6 +50,8 @@ public class PathMapOverlay extends Overlay {
             return null;
         }
 
+        mapClipArea = getWorldMapClipArea(client.getWidget(WidgetInfo.WORLD_MAP_VIEW).getBounds());
+
         if (config.drawTransports()) {
             for (WorldPoint a : plugin.pathfinder.transports.keySet()) {
                 Point mapA = worldMapOverlay.mapWorldPointToGraphicsPoint(a);
@@ -61,13 +64,13 @@ public class PathMapOverlay extends Overlay {
                     if (mapB == null) {
                         continue;
                     }
-
-                    graphics.drawLine(mapA.getX(), mapA.getY(), mapB.getX(), mapB.getY());
+                    Line2D line = clipLine(mapA.getX(), mapA.getY(), mapB.getX(), mapB.getY(), mapClipArea.getBounds());
+                    if (line != null){
+                        graphics.drawLine((int)line.getX1(), (int)line.getY1(), (int)line.getX2(), (int)line.getY2());
+                    }
                 }
             }
         }
-
-        mapClipArea = getWorldMapClipArea(client.getWidget(WidgetInfo.WORLD_MAP_VIEW).getBounds());
 
         if (plugin.currentPath == null) {
             return null;
@@ -89,6 +92,54 @@ public class PathMapOverlay extends Overlay {
         }
 
         return null;
+    }
+
+    // Liang-Barsky line clipping algorithm
+    // Returns a Line2D clipped to a Rectangle, null if the line does not intersect the rectangle
+    private Line2D clipLine(float x0, float y0, float x1, float y1, Rectangle rect){
+        Line2D line = new Line2D.Float();
+        float t0 = 0.0f;
+        float t1 = 1.0f;
+        float xDelta = x1 - x0;
+        float yDelta = y1 - y0;
+        class EdgeCheck{
+            public final float p;
+            public final float q;
+
+            EdgeCheck(float p, float q)
+            {
+                this.p = p;
+                this.q = q;
+            }
+        }
+        EdgeCheck[] edgeChecks = {
+                new EdgeCheck(-xDelta, -((float)rect.getMinX() - x0)),
+                new EdgeCheck(xDelta, ((float)rect.getMaxX() - x0)),
+                new EdgeCheck(-yDelta, -((float)rect.getMinY() - y0)),
+                new EdgeCheck(yDelta, ((float)rect.getMaxY() - y0)),
+        };
+
+        for (EdgeCheck edge : edgeChecks){
+            float r = edge.q / edge.p;
+            if (edge.p == 0 && edge.q < 0) {
+                return (null);
+            }
+            if (edge.p < 0){
+                if (r > t1) {
+                    return (null);
+                } else if (r > t0) {
+                    t0 = r;
+                }
+            } else if (edge.p > 0) {
+                if (r < t0) {
+                    return (null);
+                } else if (r < t1) {
+                    t1 = r;
+                }
+            }
+        }
+        line.setLine(x0 + t0 * xDelta, y0 + t0 * yDelta, x0 + t1 * xDelta, y0 + t1 * yDelta);
+        return (line);
     }
 
     private void drawOnMap(Graphics2D graphics, WorldPoint point, Color color) {
